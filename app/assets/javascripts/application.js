990111dae9a1a60dacc3d0a8dec252db8602e1c5
Fatmylin <jimmy0921956909@gmail.com>
2017-11-05 21:30:12 +0800

    FBtest unsuccess

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,19 @@
 //= require commodities
 //= require_tree .
 
+document.bind("fb.loaded", function() {
+  FB.getLoginStatus(function(response) {
+
+    console.log('FB STATUS: ' + response.status);
+    if(response.status == "connected") {
+
+      console.log("FB AUTHED");
+
+      location.href =
+        '/auth/facebook/callback?' +
+        $.param({ signed_request: response.authResponse.signedRequest })
+      });
+
+    }
+  });
+});
