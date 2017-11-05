74f09db1b4ced02e396c06937d01cf2f28b0cc1d
Fatmylin <jimmy0921956909@gmail.com>
2017-11-05 22:01:01 +0800

    FBtest unsuccess

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -256,9 +256,8 @@ Devise.setup do |config|
     client_options: {
       site: 'https://graph.facebook.com/v3.0',
       authorize_url: "https://www.facebook.com/v3.0/dialog/oauth",
-      :ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}
     }
-    token_params: {
+    ,token_params: {
         parse: :json
     }
 
