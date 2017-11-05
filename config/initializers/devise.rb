d43d3ccc593560b631b9905bb673949906d76571
Fatmylin <jimmy0921956909@gmail.com>
2017-11-05 20:38:35 +0800

    FBtest

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -251,7 +251,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, "APP_ID", "APP_SECRET",
+  config.omniauth :facebook, ENV["APP_ID"], ENV["APP_SECRET"],
   :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}
 
   # ==> Warden configuration
