51b243f2a9fac7afcec664576d5c4e7143226c21
Fatmylin <jimmy0921956909@gmail.com>
2017-11-07 14:20:31 +0800

    mailer test3

--- app/mailers/new_order_mailer.rb
+++ app/mailers/new_order_mailer.rb
@@ -3,8 +3,7 @@ class NewOrderMailer < ApplicationMailer
 	def new_order(user,order)
 		@user = user
 		@order = order
-		mail(to:@user.email ,subject:"《一盒》訂單成立")
-		
+		mail(to:@user.email ,subject:"《一盒》訂單成立")		
 	end
 
 end
