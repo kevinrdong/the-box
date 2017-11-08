b489b798f38b5709149883709a32b5a9d5c63234
Fatmylin <jimmy0921956909@gmail.com>
2017-11-08 22:45:18 +0800

    picture

--- app/uploaders/image4_uploader.rb
+++ app/uploaders/image4_uploader.rb
@@ -34,7 +34,7 @@ class Image4Uploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
    version :thumb do
-     process resize_to_fit: [50, 50]
+     process resize_to_fit: [10, 10]
    end
 
   # Add a white list of extensions which are allowed to be uploaded.
