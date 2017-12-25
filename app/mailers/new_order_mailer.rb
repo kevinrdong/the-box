dd1da0782c02122b351cc87b0ef2f5af00d26368
Fatmylin <jimmy0921956909@gmail.com>
2017-12-26 01:10:34 +0800

    系統提醒使用者有人購買

--- app/mailers/new_order_mailer.rb
+++ app/mailers/new_order_mailer.rb
@@ -3,7 +3,8 @@ class NewOrderMailer < ApplicationMailer
 	def new_order(user,order)
 		@user = user
 		@order = order
-		mail(to:@user.email ,subject:"《一盒》訂單成立")		
+		mail(to:@user.email ,subject:"《一盒》訂單成立")	
+		mail(to:lebox1995@gmail.com ,subject:"有人在《一盒》買都東西囉")		
 	end
 
 	def cancel_order(user,order)
